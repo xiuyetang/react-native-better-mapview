d709b390c2c9b481fdef628fb700aea1a476d632
Jay Garcia <jesusdgarcia@gmail.com>
2015-10-05 11:39:06 -0400

    Initial commit

---
+++ iOS/MCBetterMapView/MCBetterMapView.h
@@ -0,0 +1,13 @@
+//
+//  MCBetterMapView.h
+//  MCBetterMapView
+//
+//  Created by Jesus Garcia on 10/5/15.
+//  Copyright © 2015 Jesus Garcia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MCBetterMapView : NSObject
+
+@end
---
+++ iOS/MCBetterMapView/MCBetterMapView.m
@@ -0,0 +1,13 @@
+//
+//  MCBetterMapView.m
+//  MCBetterMapView
+//
+//  Created by Jesus Garcia on 10/5/15.
+//  Copyright © 2015 Jesus Garcia. All rights reserved.
+//
+
+#import "MCBetterMapView.h"
+
+@implementation MCBetterMapView
+
+@end
